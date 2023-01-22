5f979c9de54df972561de4ed19d526266f6e1845
hirakan045 <hirakan045@gmail.com>
2023-01-22 18:03:54 +0900

    ✨ index/about added title

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,23 +41,7 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+          <h1>Index Page</h1>
         </div>
         <Links />
       </main>
