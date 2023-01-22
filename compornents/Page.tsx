2b4a5aedd276f482864bdc6c406f9da52224b3c7
hirakan045 <hirakan045@gmail.com>
2023-01-22 19:26:17 +0900

    ♻️ ヘッドラインのコンポーネント化

--- compornents/Page.tsx
+++ compornents/Page.tsx
@@ -1,9 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import { Links } from '@/compornents/Links'
-import Headline from '@/compornents/Headline'
 
 const inter = Inter({ subsets: ['latin'] })
 
